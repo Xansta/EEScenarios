b295dcb5ecbe9b67c3af19e3506a92426c4bad17
starry wisdom <cult.of.starry.wisdom@gmail.com>
2020-05-09 23:32:34 +0100

    Update StarryUtil to v2

--- scenario_09_sandbox.lua
+++ scenario_09_sandbox.lua
@@ -94,6 +94,7 @@
 --									30U																								
 --  --  --  --  --  --  --  --  --  --  --  --  --  --  --  --  --  --  --  Menu Map  --  --  --  --  --  --  --  --  --  --  --  --  --  --  --  --
 require("utils.lua")
+-- starryUtil v2
 starryUtil={
 	math={
 		-- linear interpolation
@@ -110,32 +111,33 @@ starryUtil={
 		-- intended to be used via addGMMessage or print, but there may be other uses
 		-- it may be worth considering adding a function which would return an array rather than a string
 		getNumberOfObjectsString=function()
-			local counts={}
+			local all_objects=getAllObjects()
+			local object_counts={}
 			--first up we accumulate the number of each type of object
-			for _,obj in pairs(getAllObjects()) do
-				if counts[obj.typeName]==nil then
-					counts[obj.typeName]=0
+			for i=1,#all_objects do
+				local object_type=all_objects[i].typeName
+				local current_count=object_counts[object_type]
+				if current_count==nil then
+					current_count=0
 				end
-				counts[obj.typeName]=counts[obj.typeName]+1
+				object_counts[object_type]=current_count+1
 			end
 			-- we want the ordering to be stable so we build a key list
-			local sortedKeys={}
-			for key in pairs(counts) do
-				table.insert(sortedKeys, key)
+			local sorted_counts={}
+			for type in pairs(object_counts) do
+				table.insert(sorted_counts, type)
 			end
-			table.sort(sortedKeys)
+			table.sort(sorted_counts)
 			--lastly we build the output
-			local ret=""
-			for _,key in ipairs(sortedKeys) do
-				if not(ret=="") then
-					ret=ret.."\n"
-				end
-				ret=ret..key.." "..counts[key]
+			local output=""
+			for _,object_type in ipairs(sorted_counts) do
+				output=output..string.format("%s: %i\n",object_type,object_counts[object_type])
 			end
-			return ret
+			return output..string.format("\nTotal: %i",#all_objects)
 		end
-	}
+	},
 }
+
 function init()
 	updateDiagnostic = false
 	healthDiagnostic = false
@@ -1660,34 +1662,8 @@ end
 function customButtons()
 	clearGMFunctions()
 	addGMFunction("-Main From Custom",initialGMFunctions)
-	addGMFunction("Object Numbers",function()
-		addGMMessage(starryUtil.debug.getNumberOfObjectsString())
-	end)
 	addGMFunction("Object Counts",function()
-		local object_list = getAllObjects()
-		local object_counts = {}
-		for i=1,#object_list do
-			local obj = object_list[i]
-			if object_counts[obj.typeName] == nil then
-				object_counts[obj.typeName] = 1
-			else
-				object_counts[obj.typeName] = object_counts[obj.typeName] + 1
-			end
-		end
-		local sorted_counts = {}
-		for object_type in pairs(object_counts) do
-			table.insert(sorted_counts,object_type)
-		end
-		table.sort(sorted_counts)
-		local output_blob = ""
-		for _, object_type in ipairs(sorted_counts) do
-			local output_line = string.format("%s: %i",object_type,object_counts[object_type])
-			print(output_line)
-			output_blob = output_blob .. output_line .. "\n"
-		end
-		print("Total: " .. #object_list)
-		output_blob = output_blob .. "Total: " .. #object_list
-		addGMMessage(output_blob)
+		addGMMessage(starryUtil.debug.getNumberOfObjectsString())
 	end)
 end
 -------------------------------------
